692fdee4db961e8bfd8bc93c51db102f37519172
Erik Sanabria <esanabria136@gmail.com>
2016-09-11 08:14:32 -0500

    Removi pruebas de main()

--- proyectos/Proyecto_Erik_Sebastian/MonitorRAM/main.cpp
+++ proyectos/Proyecto_Erik_Sebastian/MonitorRAM/main.cpp
@@ -22,10 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <string>
 #include <memory>
-#include <iostream>
 
-using std::cout;
-using std::cin;
 using std::string;
 using std::vector;
 
