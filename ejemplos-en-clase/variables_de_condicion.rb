8097604fbaf9fc09972da9f53deffbb7802d9774
Gunnar Wolf <gwolf@gwolf.org>
2016-09-20 21:36:49 -0500

    Soluciono el problema en mi ejemplo de variables de condición. ¡Gracias a la buena vista de Alberto Díaz! :-D

--- ejemplos-en-clase/variables_de_condicion.rb
+++ ejemplos-en-clase/variables_de_condicion.rb
@@ -2,10 +2,10 @@
 # coding: utf-8
 #
 # El ejemplo mencionado en el final de la presentación 6, un ejemplo
-# NO funcional de manejo de variables de condición. ¿Puedes encontrar
-# el error?
+# NO funcional de manejo de variables de condición — Ahora
+# funcionando, gracias al trabajo en grupo y a la buena vista
+# particularmente de Alberto Díaz! :-D
 
-# coding: utf-8
 class EjemploHilos
   require 'thread'
   def initialize
@@ -19,24 +19,21 @@ class EjemploHilos
     t2 = Thread.new {f2}
     sleep 0.1; @mut.lock
     @cv.wait(@mut) while (@estado != 2)
-    puts 'run. Estado: %d' % [@estado]
     puts '%d ' % @x
     @mut.unlock
   end
   def f1
     sleep 0.1; @mut.lock
-    @cv.wait(@mut) while (@estado != 0)
+    @cv.wait(@mut) while (@estado != 1)
     @x += 3; @estado += 1
-    puts 'Saliendo de la parte crítica para F1 - %d; %d' % [@x, @estado]
-    @cv.broadcast(@mut)
+    @cv.broadcast
     @mut.unlock
   end
   def f2
     sleep 0.1; @mut.lock
-    @cv.wait(@mut) while (@estado != 1)
+    @cv.wait(@mut) while (@estado != 0)
     @x *= 2; @estado += 1
-    puts 'Saliendo de la parte crítica para F2 - %d; %d' % [@x, @estado]
-    @cv.broadcast(@mut)
+    @cv.broadcast
     @mut.unlock
   end
   e = EjemploHilos.new;10.times{e.run}
