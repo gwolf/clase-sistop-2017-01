bc505a60c71403b0b6fb7c042a841819e902d2ed
Erik Sanabria <esanabria136@gmail.com>
2016-09-23 13:33:19 -0500

    Agregando comentario

--- proyectos/Proyecto_Erik_Sebastian/MonitorRAM/ui_mainwindow.h
+++ proyectos/Proyecto_Erik_Sebastian/MonitorRAM/ui_mainwindow.h
@@ -1,4 +1,124 @@
+/**********************************************************************
+Definiciones de las clases de Qt para la ventana principal generado por Qt
+Copyright (C) 2016  Erik Sanabria esanabria136@gmail.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #ifndef UI_MAINWINDOW_H
 #define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTreeWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionAcerca_De;
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout;
+    QTreeWidget *treeWidget;
+    QHBoxLayout *horizontalLayout;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QMenu *menuAcerca_de;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(1060, 623);
+        actionAcerca_De = new QAction(MainWindow);
+        actionAcerca_De->setObjectName(QStringLiteral("actionAcerca_De"));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        verticalLayout = new QVBoxLayout(centralWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        treeWidget = new QTreeWidget(centralWidget);
+        treeWidget->setObjectName(QStringLiteral("treeWidget"));
+
+        verticalLayout->addWidget(treeWidget);
+        treeWidget->setColumnCount(4);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        horizontalLayout->addWidget(pushButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 1060, 19));
+        menuAcerca_de = new QMenu(menuBar);
+        menuAcerca_de->setObjectName(QStringLiteral("menuAcerca_de"));
+        MainWindow->setMenuBar(menuBar);
+
+        menuBar->addAction(menuAcerca_de->menuAction());
+        menuAcerca_de->addAction(actionAcerca_De);
+
+        retranslateUi(MainWindow);
+        QObject::connect(pushButton, SIGNAL(clicked()), MainWindow, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Monitor RAM", 0));
+        actionAcerca_De->setText(QApplication::translate("MainWindow", "Acerca De", 0));
+        actionAcerca_De->setShortcut(QApplication::translate("MainWindow", "F1", 0));
+        QTreeWidgetItem *___qtreewidgetitem = treeWidget->headerItem();
+        ___qtreewidgetitem->setText(3, QApplication::translate("MainWindow", "Ejecutable", 0));
+        ___qtreewidgetitem->setText(2, QApplication::translate("MainWindow", "% RAM", 0));
+        ___qtreewidgetitem->setText(1, QApplication::translate("MainWindow", "Usuario", 0));
+        ___qtreewidgetitem->setText(0, QApplication::translate("MainWindow", "PID", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "&Salir", 0));
+        menuAcerca_de->setTitle(QApplication::translate("MainWindow", "Ayuda", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
 
 #endif // UI_MAINWINDOW_H
