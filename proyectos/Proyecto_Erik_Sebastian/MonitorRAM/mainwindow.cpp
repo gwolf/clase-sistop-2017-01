3e1cf4d59d331429118662e7612e90fc87e74ef6
Erik Sanabria <esanabria136@gmail.com>
2016-09-23 15:24:32 -0500

    Descomentando codigo

--- proyectos/Proyecto_Erik_Sebastian/MonitorRAM/mainwindow.cpp
+++ proyectos/Proyecto_Erik_Sebastian/MonitorRAM/mainwindow.cpp
@@ -38,29 +38,29 @@ void MainWindow::updateTree(std::vector<Process> *data)
 {
     QTreeWidgetItem *tmp= new QTreeWidgetItem(ui->treeWidget);
 
-    for(size_t i{}; i < data->size(); ++i)
-    {
-        QString va1 = QString::fromStdString(data->at(i).get_PID());
-        QString va2 = QString::fromStdString(data->at(i).get_user());
-        QString va3 = QString::fromStdString(data->at(i).get_Mem());
-        QString va4 = QString::fromStdString(data->at(i).get_Exec());
+//    for(size_t i{}; i < data->size(); ++i)
+//    {
+//        QString va1 = QString::fromStdString(data->at(i).get_PID());
+//        QString va2 = QString::fromStdString(data->at(i).get_user());
+//        QString va3 = QString::fromStdString(data->at(i).get_Mem());
+//        QString va4 = QString::fromStdString(data->at(i).get_Exec());
 
-        tmp->setText(0, va1);
-        tmp->setText(1, va2);
-        tmp->setText(2, va3);
-        tmp->setText(3, va4);
-        ui->treeWidget->insertTopLevelItem(1, tmp);
-    }
+//        tmp->setText(0, va1);
+//        tmp->setText(1, va2);
+//        tmp->setText(2, va3);
+//        tmp->setText(3, va4);
+//        ui->treeWidget->insertTopLevelItem(1, tmp);
+//    }
 
-//    QString val1 = "PID goes here";
-//    QString val2 = "User goes here";
-//    QString val3 = "RAM percentage goes here";
-//    QString val4 = "Executable path goes here";
-//    tmp->setText(0, val1);
-//    tmp->setText(1, val2);
-//    tmp->setText(2, val3);
-//    tmp->setText(3, val4);
-//    ui->treeWidget->insertTopLevelItem(1, tmp);
+    QString val1 = "PID goes here";
+    QString val2 = "User goes here";
+    QString val3 = "RAM percentage goes here";
+    QString val4 = "Executable path goes here";
+    tmp->setText(0, val1);
+    tmp->setText(1, val2);
+    tmp->setText(2, val3);
+    tmp->setText(3, val4);
+    ui->treeWidget->insertTopLevelItem(1, tmp);
     sleep(1);
     ui->treeWidget->clear();
 }
