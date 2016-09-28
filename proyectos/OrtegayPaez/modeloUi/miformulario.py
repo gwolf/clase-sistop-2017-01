3805bb9761a779c3413718641b7e51e1bc417a9a
devlfx <laog18@gmail.com>
2016-09-28 01:31:34 -0700

    Agrego segunda version de GUI semifuncional

--- proyectos/OrtegayPaez/modeloUi/miformulario.py
+++ proyectos/OrtegayPaez/modeloUi/miformulario.py
@@ -1,37 +1,23 @@
 import sys
 from monitor import *
+from barThreads import *
 
-class MiFormulario(QtGui.QTabWidget):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.ui = Ui_TabWidget()
+
+class formu(QtGui.QTabWidget,QtGui.QMainWindow):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.ui= Ui_TabWidget()
         self.ui.setupUi(self)
-
-
-
+        self.cthread=cputhread()
+        self.rthread = ramThread()
+        self.cthread.start()
+        self.rthread.start()
+        self.connect(self.cthread, QtCore.SIGNAL('Cpu_Val'),self.ui.CpuBar.setValue)
+        self.connect(self.rthread, QtCore.SIGNAL('Mem_Val'),self.ui.RamBar.setValue)
 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    myapp = MiFormulario()
+    myapp = formu()
     myapp.show()
     sys.exit(app.exec_())
-"""
-import sys
-from priminterfaz import *
-
-
-class MiFormulario(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.ui = Ui_Dialog()
-        self.ui.setupUi(self)
-
-
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    myapp = Ejemplo()
-    myapp.show()
-    sys.exit(app.exec_())
-"""
