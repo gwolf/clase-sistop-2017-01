bce1bd223d6adbfdae226979e10ff9ed50fd40ea
DakeDekaane <servantarcherubw@gmail.com>
2016-09-29 01:52:21 -0500

    Se modifican las funciones que obtienen los datos el sistema, preparando parcialmente para su implementación con hilos

--- proyectos/YaxkinSanchez/test2.py
+++ proyectos/YaxkinSanchez/test2.py
@@ -1,87 +1,105 @@
-#!/usr/bin/python2.7
-# -*- coding: UTF-8 -*-
+#!/usr/bin/python
+# -*- Encoding: utf-8 -*-
+#
+#	MMFunciones.py
+#	Autor: David Yaxkin Sanchez Neri
+#	
+#	Script en Python que define funciones para obtener los datos del sistema.
+#	
 
+import time
 import commands
+import asciimatics
 
-def cpuModelo():
+#Variables útiles para algunas funciones
+#Número de núcleos del procesador
+no_nucleos = int(commands.getoutput("grep processor /proc/cpuinfo | wc -l"))
+#Número de funciones monitor
+no_funciones_monitor = 14;
+
+#Obtiene el modelo del CPU
+def cpuModelo(screen):
 	modelo = commands.getoutput("cat /proc/cpuinfo | grep -e 'model\ name*'")
 	modelo = modelo.replace("model name	: ","")
-	return modelo
+	screen.print_at(modelo,14,3)
 
-def cpuUso():
-	cpu_uso = commands.getoutput("top -d 1 -b -n2 | grep 'Cpu(s)'")
+#Obtiene el uso del CPU por parte del usuario
+def cpuUsuario(screen):
+	cpu_estado_1 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3; do echo $c2; done")
+	time.sleep(1)
+	cpu_estado_2 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3; do echo $c2; done")
+	cpu_uso = (int(cpu_estado_2) - int(cpu_estado_1))/no_nucleos
+	screen.print_at(str(cpu_uso) + "%",11,7)
 
- 	cpu_usuario = cpu_uso[cpu_uso.find("us")-5:cpu_uso.find("us")]
- 	cpu_sistema = cpu_uso[cpu_uso.find("sist")-5:cpu_uso.find("sist,")]
- 	cpu_inactivo = cpu_uso[cpu_uso.find("inact")-5:cpu_uso.find("inact")]
-	# eliminamos la coma decimal por un punto
-	cpu_uso=cpu_uso.replace(",",".")
- 
-	# devolvemos el % de uso
-	return float(cpu_inactivo.strip())
+#Obtiene el uso del CPU por parte del sistema
+def cpuSistema(screen):
+	cpu_estado_1 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5; do echo $c4; done")
+	time.sleep(1)
+	cpu_estado_2 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5; do echo $c4; done")
+	cpu_uso = (int(cpu_estado_2) - int(cpu_estado_1))/no_nucleos
+	screen.print_at(str(cpu_uso) + "%",41,7)
 
-def cpuUso2():
-	cpu_uso = commands.getoutput("top -d 1 -b -n2 | grep 'Cpu(s)'")
+#Obtiene el porcentaje de inactividad del CPU
+def cpuInactivo(screen):
+	cpu_estado_1 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5 c6; do echo $c5; done")
+	time.sleep(1)
+	cpu_estado_2 = commands.getoutput("cat /proc/stat | grep 'cpu ' | while read c1 c2 c3 c4 c5 c6; do echo $c5; done")
+	cpu_uso = (int(cpu_estado_2) - int(cpu_estado_1))/no_nucleos
+	screen.print_at(str(cpu_uso) + "%",72,7)
 
- 	cpu_usuario = cpu_uso[cpu_uso.find("us")-5:cpu_uso.find("us")]
- 	cpu_sistema = cpu_uso[cpu_uso.find("sist")-5:cpu_uso.find("sist,")]
- 	cpu_inactivo = cpu_uso[cpu_uso.find("inact")-5:cpu_uso.find("inact")]
-	# eliminamos la coma decimal por un punto
-	cpu_uso=cpu_uso.replace(",",".")
- 
-	# devolvemos el % de uso
-	return float(cpu_sistema.strip())
+#Obtiene el kernel del SO
+def kernel(screen):
+	version_so = commands.getoutput("cat /proc/version | while read c1 c2 c3 c4; do echo $c1 $c2 $c3; done")
+	screen.print_at(version_so,14,4)
 
-def cpuUso3():
-	cpu_uso = commands.getoutput("top -d 1 -b -n2 | grep 'Cpu(s)'")
-
- 	cpu_usuario = cpu_uso[cpu_uso.find("us")-5:cpu_uso.find("us")]
- 	cpu_sistema = cpu_uso[cpu_uso.find("sist")-5:cpu_uso.find("sist,")]
- 	cpu_inactivo = cpu_uso[cpu_uso.find("inact")-5:cpu_uso.find("inact")]
-	# eliminamos la coma decimal por un punto
-	cpu_uso=cpu_uso.replace(",",".")
- 
-	# devolvemos el % de uso
-	return float(cpu_usuario.strip())
- 
-def memTotal():
+#Obtiene la cantidad de memoria principal total
+def memTotal(screen):
 	mem_total =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '1 p'")
-	return mem_total
+	screen.print_at(mem_total,11,13)
 
-def memLibre():
+#Obtiene la cantidad de memoria principal libre
+def memLibre(screen):
 	mem_libre =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '2 p'")
-	return mem_libre
+	screen.print_at(mem_libre,42,13)
 
-def memDisponible():
-	mem_libre =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '3 p'")
-	return mem_libre
+#Obtiene la cantidad de memoria principal disponible
+def memDisponible(screen):
+	mem_disponible =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '3 p'")
+	screen.print_at(mem_disponible,42,14)
 
-def memUso():
+#Obtiene la cantidad de memoria principal en uso
+def memUso(screen):
 	mem_uso =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '7 p'")
-	return mem_uso
+	screen.print_at(mem_uso,11,14)
 
-def memSwapTotal():
+#Obtiene la cantidad de memoria swap total
+def memSwapTotal(screen):
 	mem_swap =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '19 p'")
-	return mem_swap
+	screen.print_at(mem_swap,71,13)
 
-def memSwapLibre():
-	mem_swap =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '20 p'")
-	return mem_swap
+#Obtiene la cantidad de memoria swap libre
+def memSwapLibre(screen):
+	mem_swaplibre =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '20 p'")
+	screen.print_at(mem_swaplibre,102,13)
 
-def memSwapUso():
-	mem_swap =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '6 p'")
-	return mem_swap
+#Obtiene la cantidad de memoria swap en uso
+def memSwapUso(screen):
+	mem_swapuso =  commands.getoutput("cat /proc/meminfo | while read c1 c2; do echo $c2; done | sed -n '6 p'")
+	screen.print_at(mem_swapuso,71,14)
 
-def numProcesos():
+#Obtiene el numero total de procesos
+def numProcesos(screen):
 	num_procesos = commands.getoutput("cat /proc/loadavg | grep -o '/[0-9]*'")
 	num_procesos = num_procesos[1:]
-	return num_procesos
+	screen.print_at(num_procesos,21,16)
 
-def numProcesosEjecucion():
+#Obtiene el numero de procesos en estado de ejecución
+def numProcesosEjecucion(screen):
 	num_procesos = commands.getoutput("cat /proc/loadavg | grep -o '[0-9]*/'")
 	num_procesos = num_procesos[:-1]
-	return num_procesos
+	screen.print_at(num_procesos,80,16)
 
+#Pequeña función para convertir de segundos a formato HH:MM:SS
 def calculaHora(segundos):
 	horas = segundos / 3600
 	if horas < 10:
@@ -94,28 +112,17 @@ def calculaHora(segundos):
 		segundos = '0' + str(segundos)
 	return str(horas) + ":" + str(minutos) + ":" + str(segundos)
 
-def tiempoFuncionamiento():
+#Obtiene el tiempo de funcionamiento (uptime) del sistema
+def tiempoFuncionamiento(screen):
 	t_funcionamiento = commands.getoutput("cat /proc/uptime | while read c1 c2; do echo $c1; done")
 	t_funcionamiento = int(t_funcionamiento[:-3])
-	return calculaHora(t_funcionamiento)
+	t_funcionamiento = calculaHora(t_funcionamiento)
+	screen.print_at(t_funcionamiento,28,9)
 
-def tiempoInactivo():
+#Obtiene el tiempo de inactividad del sistema
+def tiempoInactivo(screen):
 	t_inactivo = commands.getoutput("cat /proc/uptime | while read c1 c2; do echo $c2; done")
 	t_inactivo = int(t_inactivo[:-3])
-	return calculaHora(t_inactivo)
+	t_inactivo = calculaHora(t_inactivo)
+	screen.print_at(t_inactivo,19,10)
 
-print "El modelo del CPU es: " + cpuModelo()
-print "El uso de procesador (inact) es del: %.2f%%" % cpuUso()
-print "El uso de procesador (sist) es del: %.2f%%" % cpuUso2()
-print "El uso de procesador (usuario) es del: %.2f%%" % cpuUso3()
-print "La memoria total del sistema es: " + memTotal()
-print "La memoria libre del sistema es: " + memLibre()
-print "La memoria disponible del sistema es: " + memDisponible()
-print "La memoria en uso del sistema es: " + memUso()
-print "La memoria swap total del sistema es: " + memSwapTotal()
-print "La memoria swap libre del sistema es: " + memSwapLibre()
-print "La memoria swap en uso del sistema es: " + memSwapUso()
-print "El numero de procesos total es: " + numProcesos()
-print "El numero de procesos en ejecucion es: " + numProcesosEjecucion()
-print "El tiempo de funcionamiento del sistema es: " + tiempoFuncionamiento()
-print "El tiempo inactivo del sistema es: " + tiempoInactivo()
