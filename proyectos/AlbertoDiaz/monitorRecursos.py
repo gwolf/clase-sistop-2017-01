6319c3c2770602400e0b674dba5e8a032a17cab0
ad01 <albertoandcar@gmail.com>
2016-09-29 16:35:56 -0500

    Modifico funcion para obtener datos de la memoria del sistema y agrego semaforo como señalizador entre hilos

--- proyectos/AlbertoDiaz/monitorRecursos.py
+++ proyectos/AlbertoDiaz/monitorRecursos.py
@@ -4,17 +4,53 @@ import time
 import commands
 import os
 
-torniquete = threading.Semaphore(0)
+evento = threading.Semaphore(0)
 
 def obtMem():
 	threading.Thread(target=watchMem,args=[]).start()
+	return 0
+
+
 def watchMem():
-	os.system("watch free -m")
-seleccion = 2
-while seleccion != 0:
-	opciones = {'1':obtMem}
-	seleccion = raw_input('Escoge una: \n')
-	try:
-		resultado = opciones[seleccion]()
-	except:
-		print("Respuesta no valida")
+	contador = 0
+	temp = 0
+	while contador <= 10:
+		os.system("clear")
+		#os.system("cat /proc/meminfo")
+		swapTotal=commands.getoutput('cat /proc/meminfo|grep "SwapTotal:"|tr -s "'" "'"|cut -d "'" "'" -f 2')
+		swapFree=commands.getoutput('cat /proc/meminfo|grep "SwapFree:"|tr -s "'" "'"|cut -d "'" "'" -f 2')
+		memTotal=commands.getoutput('cat /proc/meminfo|grep "MemTotal:"|tr -s "'" "'"|cut -d "'" "'" -f 2')
+		memFree=commands.getoutput('cat /proc/meminfo|grep "MemFree:"|tr -s "'" "'"|cut -d "'" "'" -f 2')
+		temp = int(memTotal)/1024
+		memTotal = str(temp)
+		temp = int(memFree)/1024
+		memFree = str(temp)
+		temp = int(swapTotal)/1024
+		memOcu = str(int(memTotal) - int(memFree))
+		swapTotal = str(temp)
+		temp = int(swapFree)/1024
+		swapFree = str(temp)
+		print "Memoria Total " + memTotal + " MB \n"
+		print "Memoria Libre " + memFree + " MB \n"
+		print "Memoria Ocupada " + memOcu + " MB \n"
+		print "Swap Total: " + swapTotal + " MB \n"
+		print "Swap Libre: " + swapFree + " MB \n"
+		time.sleep(1)
+		contador+=1
+	evento.release()
+	return 0
+
+def imprimeMenu():
+	global evento
+	seleccion = '0'
+	while seleccion != '5':
+		os.system("clear")
+		opciones = {'1':obtMem}
+		seleccion = raw_input('Escoge una: \n')
+		try:
+			resultado = opciones[seleccion]()
+			evento.acquire()
+		except:
+			print("Respuesta no valida")
+		print seleccion
+imprimeMenu()
