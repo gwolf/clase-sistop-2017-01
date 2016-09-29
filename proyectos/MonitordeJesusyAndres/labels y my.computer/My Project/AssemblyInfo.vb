a6d7c1f08b4eeba2cd89cd38daf142a1964a9dae
JesusAlbertoGarciaFlores <jesus.unam.117@hotmail.com>
2016-09-29 02:59:04 -0500

    Agreo los avances 1

---
+++ proyectos/MonitordeJesusyAndres/labels y my.computer/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Information Tool")> 
+<Assembly: AssemblyDescription("Herramienta avanzada de información del sistema")> 
+<Assembly: AssemblyCompany("Infosama Technology")> 
+<Assembly: AssemblyProduct("Information Tool")> 
+<Assembly: AssemblyCopyright("Copyright ©  2011")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("0a6f990c-0347-4c5c-b841-0b8450354d28")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
