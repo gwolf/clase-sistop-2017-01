2c83a6c3bedcd0df5ede46e4c217e1734827791f
Perez Martinez David Antonio <d-a-pm@hotmail.com>
2016-09-30 00:20:47 -0500

    Se agrego scrollbar

--- proyectos/gui2.py
+++ proyectos/gui2.py
@@ -1,24 +1,41 @@
 import threading
+import time
 from Tkinter import *
-import datetime
+import re
+
+def myfunction(event):
+    canvas.configure(scrollregion=canvas.bbox("all"),width=600,height=600)
 
 root = Tk()
+sizex = 800
+sizey = 600
+posx  = 100
+posy  = 100
+root.wm_geometry("%dx%d+%d+%d" % (sizex, sizey, posx, posy))
+
+myframe=Frame(root,relief=GROOVE,width=50,height=100,bd=1)
+myframe.place(x=10,y=10)
+
+canvas=Canvas(myframe)
+frame=Frame(canvas)
+myscrollbar=Scrollbar(myframe,orient="vertical",command=canvas.yview)
+canvas.configure(yscrollcommand=myscrollbar.set)
+
+myscrollbar.pack(side="right",fill="y")
+canvas.pack(side="left")
+canvas.create_window((0,0),window=frame,anchor='nw')
+frame.bind("<Configure>",myfunction)
 
 update = threading.Semaphore(0)
 
-scale = Scale(root, from_=0, to=42)
-scale.pack()
-
 def mem():
-    update.acquire()
     memState = open("/proc/meminfo","r")
     memInfo = memState.read()
     memState.close()
-    diskLabel = Label(root,text=memInfo,justify=LEFT)
-    
+    memStats = Label(frame,text=memInfo,justify=LEFT).grid(row=1,column=0)
     
 
-def trigger():
+def monitor():
  threading.Thread(target=disk,args=[]).start()
  threading.Thread(target=mem,args=[]).start()
 
@@ -26,14 +43,13 @@ def disk():
     discState = open("/proc/diskstats","r")
     infoDisk = discState.read()
     discState.close()
-    diskState(infoDisk)
+    diskStates(infoDisk)
 
-def diskState(infoDisk):
-    diskLabel = Label(root,text=infoDisk,justify=LEFT)
-    update.release()
-    root.after(1000,trigger)
+def diskStates(infoDisk):
+    diskLabel = Label(frame,text=infoDisk,justify=LEFT).grid(row=0,column=0)
+    root.after(1000,monitor)
 
-threading.Thread(target=trigger,args=[]).start()
 
+threading.Thread(target=monitor,args=[]).start()
 
 root.mainloop()
