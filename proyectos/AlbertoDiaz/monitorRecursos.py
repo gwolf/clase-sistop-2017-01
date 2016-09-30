b6a6eaf84429b003199bf0b484d8b439cb381e40
ad01 <albertoandcar@gmail.com>
2016-09-29 23:38:02 -0500

    Agrego funcion para revisar caracteristicas de Procesador

--- proyectos/AlbertoDiaz/monitorRecursos.py
+++ proyectos/AlbertoDiaz/monitorRecursos.py
@@ -11,7 +11,7 @@ def obtMem():
 	return 0
 
 
-def watchMem():
+def hiloMem():
 	contador = 0
 	temp = 0
 	while contador <= 10:
@@ -40,17 +40,53 @@ def watchMem():
 	evento.release()
 	return 0
 
-def imprimeMenu():
+def hiloVersion():
+	threading.Thread(target=watchVersion,args=[]).start()
+	return 0
+
+def watchVersion():
+	contador = 0
+	version = commands.getoutput('cat /proc/version')
+	while contador <= 10:
+		os.system("clear")
+		print version
+		time.sleep(1)
+		contador+=1
+	evento.release()
+	return 0
+
+def hiloProc():
+	threading.Thread(target=watchProc,args=[]).start()
+	return 0
+
+def watchProc():
+	contador = 0
+	numProc = commands.getoutput("grep processor /proc/cpuinfo | wc -l")
+	numNucleos = commands.getoutput("cat /proc/cpuinfo | grep \"cpu cores\"")
+	modelo = commands.getoutput("cat /proc/cpuinfo | grep \"model name\"")
+	while contador <= 10:
+		os.system("clear")
+		print "Numero de Procesadores : " + numProc
+		print numNucleos
+		print modelo
+		time.sleep(1)
+		contador+=1
+	evento.release()
+	return 0
+def Menu():
 	global evento
 	seleccion = '0'
-	while seleccion != '5':
+	while seleccion != '4':
 		os.system("clear")
-		opciones = {'1':obtMem}
-		seleccion = raw_input('Escoge una: \n')
+		opciones = {'1':hiloMem,'2':hiloVersion,'3':hiloProc}
+		print "Escoge una de las siguientes opciones:\n1.- Obtener informacion sobre RAM Y SWAP\n2.- Obtener informacion sobre la version de linux."
+		print "3.- Informacion sobre el procesador(Numero de Procesadores, Numero de Nucleos, Modelo del Procesador)\n4.Salir"
+		seleccion = raw_input('\nEscribe tu opcion: \n')
 		try:
 			resultado = opciones[seleccion]()
 			evento.acquire()
 		except:
-			print("Respuesta no valida")
-		print seleccion
-imprimeMenu()
+			if seleccion != '4':
+				print("Respuesta no valida")
+		#print seleccion
+Menu()
