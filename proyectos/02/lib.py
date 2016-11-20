55af6224f70c683b812dba063ba57b7c9ef62dc7
maxSanchez <slemuh.ms@gmail.com>
2016-11-19 19:06:46 -0600

    Este es el primer avance del proyecto 2

---
+++ proyectos/02/lib.py
@@ -0,0 +1,18 @@
+#esta es la biblioteca del micro sistema
+import os
+def impPresentacion():
+	print("Micro sistema de archivos SanPer, creado por Max Armando Sanchez Hernandez y Perez Martinez David Antonio ")
+	print("Escriba help para ayuda ")
+def lsd(comando):
+	print(comando)
+def add(comando):
+	print(comando)
+def new(comando):
+	print(comando)
+def dele(comando):
+	print(comando)
+def cat(comando):
+	print(comando)
+def hel(comando):
+	os.system("cat help.txt")
+	print("\n")
