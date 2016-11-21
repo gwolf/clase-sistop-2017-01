babc879efcab27975b484e47beaac0aa419f58e1
penserbjorne <penserbjorne@users.noreply.github.com>
2016-11-20 18:48:02 -0600

    Se añade la estructura base del proyecto.

---
+++ proyectos/02/Penserbjorne/DummyFileSystem/include/Draw.h
@@ -0,0 +1,16 @@
+#ifndef DRAW_H
+#define DRAW_H
+
+
+class Draw
+{
+    public:
+        Draw();
+        virtual ~Draw();
+
+        void drawDFS();
+    protected:
+    private:
+};
+
+#endif // DRAW_H
---
+++ proyectos/02/Penserbjorne/DummyFileSystem/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Draw.h>
+
+using namespace std;
+
+int main()
+{
+    Draw artist;
+    artist.drawDFS();
+    return 0;
+}
