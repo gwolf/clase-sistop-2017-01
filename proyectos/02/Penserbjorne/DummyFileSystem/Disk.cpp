14f0d1bc2c84ae26a23879bad79e20a39e160b2c
penserbjorne <penserbjorne@users.noreply.github.com>
2016-11-21 03:54:13 -0600

    se añadio la base para la clase Disk

---
+++ proyectos/02/Penserbjorne/DummyFileSystem/Disk.cpp
@@ -0,0 +1,14 @@
+/*
+*   Autor: Paul Sebastian Aguilar Enriquez a.k.a Penserbjorne
+*   Descripcion: Clase base para simular un disco
+*/
+
+#include "Disk.h"
+
+Disk::Disk(){
+    //ctor
+}
+
+Disk::~Disk(){
+    //dtor
+}
---
+++ proyectos/02/Penserbjorne/DummyFileSystem/Disk.h
@@ -0,0 +1,18 @@
+/*
+*   Autor: Paul Sebastian Aguilar Enriquez a.k.a Penserbjorne
+*   Descripcion: Clase base para simular un disco
+*/
+
+#ifndef DISK_H
+#define DISK_H
+
+class Disk
+{
+    public:
+        Disk();
+        virtual ~Disk();
+    protected:
+    private:
+};
+
+#endif // DISK_H
