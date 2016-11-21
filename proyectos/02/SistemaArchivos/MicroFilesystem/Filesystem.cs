15e97532f0580d3f96de36e58146fd28113f971f
Angel <angel_betta@outlook.com>
2016-11-20 19:28:17 -0600

    El programa ahora reconoce todos los comandos que se pueden utilizar y corrige algunos errores usando regex.

--- proyectos/02/SistemaArchivos/MicroFilesystem/Filesystem.cs
+++ proyectos/02/SistemaArchivos/MicroFilesystem/Filesystem.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MicroFilesystem
@@ -23,31 +24,213 @@ namespace MicroFilesystem
 
             if(File.Exists(filesystemPath + "FileSystem.zip"))
             {
-                Console.WriteLine(filesystemPath);
-                Console.WriteLine("El archivo FileSystem.zip existe!");
-                Console.ReadKey();
+                //ZipFile.ExtractToDirectory(filesystemPath + "FileSystem.zip", filesystemPath + "FileSystem");
             }
             else
             {
                 Console.WriteLine("No se encontró FileSystem.zip.");
-                Console.WriteLine("PRESIONA ENTER PARA TERMINAR EL PROGRAMA");
+                Console.Write("PRESIONA ENTER PARA TERMINAR EL PROGRAMA");
                 Console.ReadKey();
                 Environment.Exit(-1);
             }
            
             commands = new List<string>();
+            history = new List<string>();
             InitializeCommands();
         }
 
         public void run()
         {
+            do
+            {
+                Console.Write("mfs> ");
+                userInput = Console.ReadLine();
+                string[] input = Regex.Replace(userInput.Trim(' '), @"\s+", " ").Split(' ');
 
+                if(commands.Contains(input[0]))
+                {
+                    history.Add(input[0]);
+                }
+
+                switch(input[0])
+                {
+                    /* case "test":
+                        Console.WriteLine("[{0}]", userInput);
+                        foreach (string c in input)
+                        {
+                            Console.WriteLine("\t >>[{0}]", c);
+                        }
+                        break;*/ 
+                    case "createfile":
+                        Console.WriteLine("createfile");
+                        break;
+                    case "read":
+                        Console.WriteLine("read");
+                        break;
+                    case "writeto":
+                        Console.WriteLine("writeto");
+                        break;
+                    case "list":
+                        Console.WriteLine("list");
+                        break;
+                    case "help":
+                        Console.WriteLine("help");
+                        break;
+                    case "history":
+                        Console.WriteLine("history llamada");
+                        break;
+                    case "remove":
+                        Console.WriteLine("remove");
+                        break;
+                    case "exit":
+                        Console.WriteLine("EXIT");
+                        break;
+                    case "":
+                        break;
+                    default:
+                        Console.WriteLine("\tIngresa un comando válido.");
+                        break;
+                }
+
+            }
+            while (userInput != "exit");
+
+            /*do
+            {
+                Console.Write(RutaActual + "> ");
+                EntradaDelTeclado = Console.ReadLine();
+                string[] entrada = EntradaDelTeclado.Split(' ');
+
+                if (comandosValidos.Contains(entrada[0].ToLower()))
+                    historial.Add(entrada[0].ToLower());
+
+                switch (entrada[0].ToLower())
+                {
+                    case "dir":
+                        if (entrada.Length == 1)
+                            Dir();
+                        else
+                            Dir(entrada);
+                        break;
+                    case "cd":
+                        try
+                        {
+                            Cd(entrada);
+                        }
+                        catch (NotEnoughParametersException nepe)
+                        {
+                            Console.WriteLine("\tError: " + nepe.Message);
+                        }
+                        break;
+                    case "touch":
+                        try
+                        {
+                            Touch(entrada);
+                        }
+                        catch (NotEnoughParametersException nepe)
+                        {
+                            Console.WriteLine("\tError: " + nepe.Message);
+                        }
+                        catch (FileAlreadyExistsException faee)
+                        {
+                            Console.WriteLine("\tError: " + faee.Message);
+                        }
+                        break;
+                    case "move":
+                        try
+                        {
+                            Move(entrada);
+                        }
+                        catch (NotEnoughParametersException nepe)
+                        {
+                            Console.WriteLine("\tError: " + nepe.Message);
+                        }
+                        break;
+                    case "cls":
+                        Cls();
+                        break;
+                    case "history":
+                        History();
+                        break;
+                    case "exit":
+                        Exit(5);
+                        break;
+                    default:
+                        Console.WriteLine("\tIngresa un comando válido");
+                        break;
+                }
+            }
+            while (EntradaDelTeclado.ToLower() != "exit"); */
+
+            /*Console.WriteLine("El archivo zip original se eliminará...");
+            Console.ReadKey();
+            File.Delete(filesystemPath + "FileSystem.zip");
+            Console.WriteLine("El archivo zip nuevo se está generando...");
+            Console.ReadKey();
+            ZipFile.CreateFromDirectory(filesystemPath + "FileSystem", filesystemPath + "FileSystem.zip");
+            Console.WriteLine("El archivo temporal está a punto de destruirse...");
+            Console.ReadKey();
+            Directory.Delete(filesystemPath + "FileSystem", true);*/
+        }
+
+        public void CreateFile()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void WriteTo()
+        {
+
+        }
+
+        public void List()
+        {
+
+        }
+
+        public void Help()
+        {
+
+        }
+
+        public void History()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void Exit()
+        {
+            //  El archivo zip utilizado originalmente se borra.
+            File.Delete(filesystemPath + "FileSystem.zip");
+            
+            //  Se crea un nuevo archivo ZIP a partir de los datos modificados en tiempo de ejecución.
+            ZipFile.CreateFromDirectory(filesystemPath + "FileSystem", filesystemPath + "FileSystem.zip");
+            
+            //  Se elimina el directorio temporal utilizado en tiempo de ejecución.
+            Directory.Delete(filesystemPath + "FileSystem", true);
+
+            Environment.Exit(0);
         }
 
         private void InitializeCommands()
         {
             commands.Add("createfile");     //  Permite crear archivos nuevos
-            commands.Add("createdir");      //  Permite crear subdirectorios nuevos
+            commands.Add("clear");          //  Limpiar la pantalla
             commands.Add("read");           //  Leer contenido de archivos
             commands.Add("writeto");        //  Escribir en archivos
             commands.Add("list");           //  Listar subdirectorios y archivos
