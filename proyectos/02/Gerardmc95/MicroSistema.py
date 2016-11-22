ccfa8b4636a5746a70750210719ef1f7ed4c6355
Gerardmc95 <gerardmc95@gmail.com>
2016-11-22 07:30:23 -0600

    Incluyo funciones de formato, para darle buena presentacion en terminal

--- proyectos/02/Gerardmc95/MicroSistema.py
+++ proyectos/02/Gerardmc95/MicroSistema.py
@@ -1,10 +1,10 @@
 #Mendoza Colmenares Gerardo
 #Segundo proyecto basado en creación de archivos a partir de un zip
 
-#En este tercer archivo se comentará el cuerpo de las funciones para saber como se hace todo
+#En este cuarto archivo se hará uso de funciones para poner más presentación a la hora de ejecución
 
 #CORRECCIONES
-#Explicar el uso de la función
+#Ninguna
 
 
 #!/usr/bin/python
@@ -35,7 +35,8 @@ def main():
 		print("\tnewdir, newfile, removedir, removefile\nedit, read, goin, goback, list-items\nexit, help")
 		print("|----------==========----------==========----------|")
 		#Mientras no pida salir, la terminal seguirá pidiendo comandos
-		while ciclo != 1:		
+		while ciclo != 1:
+			fuente_normal()
 			comando = input("root@fi_unam$ ")
 			if comandos(comando):
 				diccionarioComandos[comando]()
@@ -55,16 +56,33 @@ def main():
 		print("¡Archivo no encontrado!\n")
 
 #----------------------------------------------------------------------------------------------------------------------------
-#AÑADIR DESCRIPCIÓN AL CUERPO DE LA FUNCIÓN
+#
 #----------------------------------------------------------------------------------------------------------------------------
+
+#Fuentes para presentacion en terminal
+def fuente_error():
+    print (chr(27)+"[1;31m")
+
+def fuente_exito():
+    print (chr(27)+"[1;33m"+"¡ÉXITO!")
+
+def fuente_normal():
+    print (chr(27)+"[0;37m")
+
+def fuente_pregunta():
+	print (chr(27)+"[3;34m")
+
+
 
 #Descomprimir el zip existente
 def unzip(file_zip):
 	#Revisamos si existe el zip
 	if len(file_zip) > 0:
 		os.system("unzip WOLF.zip")
+		fuente_exito()
 		print("¡Se descomprimio el zip correctamente!")
 	else:
+		fuente_error()
 		print("No existe el zip WOLF!")
 
 #Verificar que comando se mando a llamar
@@ -77,6 +95,7 @@ def comandos(comando):
 
 #Agrega una carpeta a WOLF
 def nuevaCarpeta():
+	fuente_pregunta()
 	print("¿Cómo se va a llamar la carpeta?\nMáximo 12 caracteres")
 	user_input = input(">> ")
 	#'getcwd' para conocer el directorio actual
@@ -85,14 +104,18 @@ def nuevaCarpeta():
 	if len(user_input) < 12:
 		#Creamos el directorio solicitado con makedirs
 		if not os.path.exists(user_input):
+			fuente_exito()
 			os.makedirs(user_input)
 		else:
+			fuente_error()
 			print("¡La carpeta ya existe!")
 	else:
+		fuente_error()
 		print("¡Tu carpeta excede los 12 caracteres!")
 
 #Agrega un archivo a WOLF
 def nuevoArchivo():
+	fuente_pregunta()
 	print("¿Cómo se va a llamar archivo?\nMáximo 10 caracteres")
 	user_input = input(">> ")
 	#Comprobamos que la longitud permitida sea correcta
@@ -101,13 +124,17 @@ def nuevoArchivo():
 		if not os.path.exists(user_input):
 			f = open(user_input,'w')
 			f.close()
+			fuente_exito()
 		else:
+			fuente_error()
 			print("¡El archivo ya existe!")	
 	else:
+		fuente_error()
 		print("¡Tu archivo excede los 10 caracteres!")
 
 #Borra una carpeta de WOLF
 def eliminarCarpeta():
+	fuente_pregunta()
 	print("¿Cómo se llama la carpeta a borrar?")
 	user_input = input(">> ")
 	#'getcwd' para conocer el directorio actual
@@ -115,13 +142,16 @@ def eliminarCarpeta():
 	#Comprobamos exista la carpeta a borrar
 	if os.path.isdir(path+'/'+user_input):
 		shutil.rmtree(user_input)
+		fuente_exito()
 	else:
+		fuente_error()
 		print("¡La carpeta no existe!")
 		#'pass' para seguir adelante sin eejecutar nada
 		pass
 
 #Borra un archivo de una carpeta de WOLF
 def eliminarArchivo():
+	fuente_pregunta()
 	print("¿Qué archivo deseas borrar?")
 	file = input(">> ")
 	#Con 'grep'  buscamos dentro de los archivos las líneas que concuerdan con un patrón
@@ -131,11 +161,14 @@ def eliminarArchivo():
 	find_file = archivo.readline()
 	if len(find_file) > 0:
 		os.system("rm -rf " + find_file)
+		fuente_exito()
 	else:
+		fuente_error()
 		print("¡El archivo no existe!")
 
 #Modifica un archivo de WOLF
 def editar():
+	fuente_pregunta()
 	print("¿Cómo se llama el archivo que quieres modificar? ")
 	user_input = input(">> ")
 	os.system("ls | grep "+ user_input +" > tmp")
@@ -144,27 +177,34 @@ def editar():
 	find_file = archivo.readline()
 	if len(find_file) > 0:
 		os.system("vi " + find_file)
+		fuente_exito()
 	else:
+		fuente_error()
 		print("¡El archivo no existe!")
 
 #Abre un archivo de solo lectura
 def leer():
+	fuente_pregunta()
 	print("¿Cómo se llama el archivo que quieres leer?")
 	user_input = input(">> ")
 	if len(user_input) < 6:
 		#Con 'cat' concatenamos archivos para posteriormente desplegarlos en pantalla
 		if os.path.exists(user_input):
+			fuente_exito()
 			print("El Archivo '"+user_input+"' contiene: ")
 			print("====================================")
 			os.system("cat " + user_input)
 			print("====================================")
 		else:
+			fuente_error()
 			print("¡El archivo no existe!")	
 	else:
+		fuente_error()
 		print("Tu achivo tiene más de 6 caracteres!")
 
 #Dirige a una carpeta específica
 def ir_a():
+	fuente_pregunta()
 	global dentroDe
 	print("¿Cómo se llama la carpeta a la que quieres entrar? ")
 	user_input = input(">> ")
@@ -175,26 +215,33 @@ def ir_a():
 		#Buscamos la carpeta a entrar
 		os.chdir(find_file[:-1])
 		dentroDe += 1
+		fuente_exito()
 	else:
+		fuente_error()
 		print("¡La carpeta no existe!")
 
 #Regresa un directorio
 def regresar_a():
+	fuente_pregunta()
 	global dentroDe
 	if dentroDe > 0:
+		fuente_exito()
 		#Regresamos un directorio con '..'
 		os.chdir("..")
 		dentroDe -= 1
 	else:
+		fuente_error()
 		print("Imposible, estas en la raíz")
 
 #Enlista los archivos de la carpeta en la que se esta ubicado
 def enlistar_archivos():
+	fuente_exito()
 	#Con '--color=always" se colorean aquellos que ya se habían creado anteriormente
 	os.system("ls --color=always")
 
 #Imprime lo que cada comando realiza
 def ayuda():
+	fuente_pregunta()
 	return 0
 
 def salir():
